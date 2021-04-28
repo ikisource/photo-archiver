0a7175af6782356074db29653e1a10158e3ce24b
Olivier MATHE <omathe@free.fr>
2021-04-28 09:36:59 +0200

    test token

--- src/main/java/omathe/gui/Main.java
+++ src/main/java/omathe/gui/Main.java
@@ -12,17 +12,17 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    public static void main(final String[] args) {
-        launch(args);
-    }
+	public static void main(final String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(final Stage stage) throws Exception {
+	@Override
+	public void start(final Stage stage) throws Exception {
 
-        final BorderPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+		final BorderPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
 
-        stage.setTitle("Photo-archiver");
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
+		stage.setTitle("Photo-archiver");
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
 }
